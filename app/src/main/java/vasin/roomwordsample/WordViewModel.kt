053dc4addce2a622493cfc1a5b9436a544da5ace
Vasin W <vasinwr@gmail.com>
2018-12-08 18:17:44 +0000

    view model added

---
+++ app/src/main/java/vasin/roomwordsample/WordViewModel.kt
@@ -0,0 +1,33 @@
+package vasin.roomwordsample
+
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
+import android.arch.lifecycle.LiveData
+import kotlinx.coroutines.experimental.*
+import kotlinx.coroutines.experimental.android.Main
+import kotlin.coroutines.experimental.CoroutineContext
+
+class WordViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository: WordRepository
+    val allWords: LiveData<List<Word>>
+
+    private var parentJob = Job()
+    private val coroutineContext: CoroutineContext
+        get() = parentJob + Dispatchers.Main
+    private val scope = CoroutineScope(coroutineContext)
+
+    init {
+        val wordsDao = WordRoomDatabase.getDatabase(application).wordDao()
+        repository = WordRepository(wordsDao)
+        allWords = repository.allWords
+    }
+
+    fun insert(word: Word) = scope.launch(Dispatchers.IO) {
+        repository.insert(word)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        parentJob.cancel()
+    }
+}
