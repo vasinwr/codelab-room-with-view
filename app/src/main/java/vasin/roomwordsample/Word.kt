974eea30f9e8ea36890fd14f573842fe4cb1af48
Vasin W <vasinwr@gmail.com>
2018-12-08 17:48:01 +0000

    word entity made

--- app/src/main/java/vasin/roomwordsample/Word.kt
+++ app/src/main/java/vasin/roomwordsample/Word.kt
@@ -1,3 +1,20 @@
 package vasin.roomwordsample
 
-data class Word(val word: String)
+import android.arch.lifecycle.LiveData
+import android.arch.persistence.room.*
+
+@Entity(tableName = "word_table")
+class Word(@PrimaryKey @ColumnInfo(name = "word") val word: String)
+
+@Dao
+interface WordDao {
+
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    fun getAllWords(): LiveData<List<Word>>
+
+    @Insert
+    fun insert(word: Word)
+
+    @Query("DELETE FROM word_table")
+    fun deleteAll()
+}
