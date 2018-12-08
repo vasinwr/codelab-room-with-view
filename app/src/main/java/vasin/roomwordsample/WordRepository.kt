4714e945be374f29f672191c7786ec7adcc63888
Vasin W <vasinwr@gmail.com>
2018-12-08 18:00:53 +0000

    repository added

---
+++ app/src/main/java/vasin/roomwordsample/WordRepository.kt
@@ -0,0 +1,13 @@
+package vasin.roomwordsample
+
+import android.arch.lifecycle.LiveData
+import android.support.annotation.WorkerThread
+
+class WordRepository(private val wordDao: WordDao) {
+    val allWords: LiveData<List<Word>> = wordDao.getAllWords()
+
+    @WorkerThread
+    suspend fun insert(word: Word) {
+        wordDao.insert(word)
+    }
+}
